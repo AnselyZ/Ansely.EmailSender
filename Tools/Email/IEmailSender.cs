aa6a4a066e176d03cb13ae879b0b8ec5bcf6b188
Ansely <943292195@qq.com>
2023-01-30 13:23:10 +0800

    添加项目文件。

---
+++ Tools/Email/IEmailSender.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ansely.Email
+{
+    /// <summary>
+    /// Provides the interface for sending email.
+    /// </summary>
+    public interface IEmailSender
+    {
+        /// <summary>
+        /// Send an email to the specified email address.
+        /// </summary>
+        /// <param name="template">An email message template.</param>
+        /// <param name="addressees">A list of specified addresses to receive mail.</param>
+        /// <returns>The result of sending the email.</returns>
+        EmailSenderResult Send(AbstractEmailTemplate template, List<string> addressees);
+
+        /// <summary>
+        /// Send an email to the specified email address.
+        /// </summary>
+        /// <typeparam name="Template">A type of the email message template.</typeparam>
+        /// <param name="addressees">A list of specified addresses to receive mail.</param>
+        /// <returns>The result of sending the email.</returns>
+        EmailSenderResult Send<Template>(List<string> addressees) where Template : AbstractEmailTemplate, new();
+
+        /// <summary>
+        /// Send an email to the specified email address as an asynchronous opration.
+        /// </summary>
+        /// <param name="template">An email message template.</param>
+        /// <param name="addressees">A list of specified addresses to receive mail.</param>
+        /// <returns>The result of sending the email.</returns>
+        Task<EmailSenderResult> SendAsync(AbstractEmailTemplate template, List<string> addressees);
+
+        /// <summary>
+        /// Send an email to the specified email address as an asynchronous opration.
+        /// </summary>
+        /// <typeparam name="Template">A type of the email message template.</typeparam>
+        /// <param name="addressees">A list of specified addresses to receive mail.</param>
+        /// <returns>The result of sending the email.</returns>
+        Task<EmailSenderResult> SendAsync<Template>(List<string> addressees) where Template : AbstractEmailTemplate, new();
+
+        
+    }
+}
